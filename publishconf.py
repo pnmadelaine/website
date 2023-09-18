0d2b96704cb099c1b64d83dc10836fb45290c669
Paul-Nicolas Madelaine <pnm@pnm.tf>
2023-09-18 14:21:50 +0200

    disable atom feed

--- publishconf.py
+++ publishconf.py
@@ -10,9 +10,6 @@ from pelicanconf import *
 SITEURL = 'https://www.pnm.tf'
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
-
 DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
