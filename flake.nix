d2ca5233582dd259db2498263afcf850dacf30a0
Paul-Nicolas Madelaine <pnm@pnm.tf>
2023-09-18 14:05:24 +0200

    add dev shell

--- flake.nix
+++ flake.nix
@@ -7,12 +7,15 @@
 
   outputs = { self, nixpkgs, flake-utils, }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
+      let
+        pkgs = import nixpkgs { inherit system; };
+        python = pkgs.python3.withPackages (ps: with ps; [ markdown pelican ]);
       in {
         packages.default = pkgs.writeTextDir "index.html" ''
           Hello world!
           This is the personal website of Paul-Nicolas Madelaine.
         '';
+        devShells.default = pkgs.mkShell { packages = [ python ]; };
       });
 
 }
