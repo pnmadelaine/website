f20726136b227c772134523ad3e88b095218c819
Paul-Nicolas Madelaine <pnm@pnm.tf>
2023-09-20 00:27:17 +0200

    format using alejandra

--- flake.nix
+++ flake.nix
@@ -1,29 +1,30 @@
 {
-
   inputs = {
     nixpkgs.url = "nixpkgs";
     flake-utils.url = "flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        python = pkgs.python3.withPackages (ps:
-          with ps; [
-            markdown
-            (pelican.overrideAttrs (_: _: { patches = [ ./pelican.patch ]; }))
-          ]);
-        website = pkgs.stdenv.mkDerivation {
-          name = "website";
-          src = ./.;
-          nativeBuildInputs = [ python ];
-          buildFlags = [ "publish" ];
-          installPhase = "cp -r output $out";
-        };
-      in {
-        packages.default = website;
-        devShells.default = pkgs.mkShell { packages = [ python ]; };
-      });
-
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      python = pkgs.python3.withPackages (ps:
+        with ps; [
+          markdown
+          (pelican.overrideAttrs (_: _: {patches = [./pelican.patch];}))
+        ]);
+      website = pkgs.stdenv.mkDerivation {
+        name = "website";
+        src = ./.;
+        nativeBuildInputs = [python];
+        buildFlags = ["publish"];
+        installPhase = "cp -r output $out";
+      };
+    in {
+      packages.default = website;
+      devShells.default = pkgs.mkShell {packages = [python];};
+    });
 }
