9b2f42e76944473be269a508c407699d310b81cc
Paul-Nicolas Madelaine <pnm@pnm.tf>
2023-09-21 02:30:53 +0200

    generate sitemap

--- flake.nix
+++ flake.nix
@@ -15,12 +15,26 @@
         with ps; [
           markdown
           (pelican.overrideAttrs (_: _: {patches = [./pelican.patch];}))
+          (
+            buildPythonPackage rec {
+              pname = "pelican-sitemap";
+              version = "1.1.0";
+              src = pkgs.fetchFromGitHub {
+                owner = "pelican-plugins";
+                repo = "sitemap";
+                rev = version;
+                sha256 = "sha256-GEOLM4wc25VLRZ4qpOENTV6YGfYPHeu95gGdBauJswA=";
+              };
+              format = "pyproject";
+              propagatedBuildInputs = [poetry-core];
+            }
+          )
         ]);
       website = pkgs.stdenv.mkDerivation {
         name = "website";
         src = ./.;
-        nativeBuildInputs = [python];
-        buildFlags = ["publish"];
+        nativeBuildInputs = [pkgs.libfaketime python];
+        buildPhase = "faketime @${builtins.toString self.lastModified} make publish";
         installPhase = "cp -r output $out";
       };
     in {
