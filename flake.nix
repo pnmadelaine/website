d7dd5916d1d1b500690b3af581b1a1b99ca96d28
Paul-Nicolas Madelaine <pnm@pnm.tf>
2024-09-09 09:57:32 +0200

    update dependencies

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs";
-    flake-utils.url = "flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
